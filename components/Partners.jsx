2ec5749703fc8ed704c8d1b3223c33c356d77561
Rhythm Saha <rs.2001.saha@gmail.com>
2024-04-05 17:14:27 +0530

    Update Partners.jsx

--- components/Partners.jsx
+++ components/Partners.jsx
@@ -3,38 +3,43 @@ import Image from "next/image";
 const Partners = () => {
     return (
         <section className="mt-28 w-[85%] max-w-6xl mx-auto grid lg:grid-cols-5 place-items-center gap-16">
-            <Image
+            <img
                 src="/images/logo-google.png"
                 alt="google"
                 objectFit="contain"
+                className="object-contain"
                 height={49}
                 width={165}
             />
-            <Image
+            <img
                 src="/images/logo-ibm.png"
                 alt="ibm"
                 objectFit="contain"
+                className="object-contain"
                 height={49}
                 width={165}
             />
-            <Image
+            <img
                 src="/images/logo-microsoft.png"
                 alt="microsoft"
                 objectFit="contain"
+                className="object-contain"
                 height={49}
                 width={165}
             />
-            <Image
+            <img
                 src="/images/logo-hp.png"
                 alt="hp"
                 objectFit="contain"
+                className="object-contain"
                 height={49}
                 width={165}
             />
-            <Image
+            <img
                 src="/images/logo-vector-graphics.png"
                 alt="vector"
                 objectFit="contain"
+                className="object-contain"
                 height={49}
                 width={165}
             />
